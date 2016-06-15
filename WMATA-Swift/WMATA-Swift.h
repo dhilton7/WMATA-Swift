826d564ba3ba5544ba8644cd9524cc1c79c9b9cf
Dan Hilton <danhilton@Dans-MacBook-Pro.local>
2016-06-15 10:19:51 -0400

    initial commit with xcode project

---
+++ WMATA-Swift/WMATA-Swift.h
@@ -0,0 +1,19 @@
+//
+//  WMATA-Swift.h
+//  WMATA-Swift
+//
+//  Created by Dan Hilton on 6/15/16.
+//  Copyright © 2016 Dan Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WMATA-Swift.
+FOUNDATION_EXPORT double WMATA_SwiftVersionNumber;
+
+//! Project version string for WMATA-Swift.
+FOUNDATION_EXPORT const unsigned char WMATA_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WMATA_Swift/PublicHeader.h>
+
+
