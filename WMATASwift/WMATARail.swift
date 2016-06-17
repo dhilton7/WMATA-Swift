efa2df68087b6407183506b9a9825c34b30d1d42
Dan Hilton <dhilton@missiondata.com>
2016-06-17 12:06:46 -0400

    complete documentation comments

--- WMATASwift/WMATARail.swift
+++ WMATASwift/WMATARail.swift
@@ -14,7 +14,7 @@ public extension WMATASwift {
     /**
         Get Next Train for a station code
         
-        - parameter StationCodes- Comma separated list of station codes
+        - parameter (String?) StationCodes: Comma separated list of station codes
     */
     public func getNextTrain(stationCodes: String?, success: ([Train])->Void, failure:(NSError)->Void) {
         self.jsonGetRequestWithPath("StationPrediction.svc/json/GetPrediction/\(stationCodes ?? "All")", success: { (json:JSON) in
@@ -28,6 +28,9 @@ public extension WMATASwift {
             }, failure: failure)
     }
     
+    /**
+        Get Train Lines
+    */
     public func getTrainLines(success: ([Line])->Void, failure:(NSError)->Void) {
         self.jsonGetRequestWithPath("Rail.svc/json/jLines", success: { (json:JSON) in
             if let linesJson = json["Lines"].array {
@@ -40,6 +43,12 @@ public extension WMATASwift {
             }, failure: failure)
     }
     
+    /**
+        Get Stops For Line
+        
+        - parameter (String?) lineCode: Line code to get stations for
+     
+    */
     public func getStopsForLine(lineCode: String?, success: ([Station])->Void, failure:(NSError)->Void) {
         let path = lineCode == nil ? "Rail.svc/json/jStations" : "Rail.svc/json/jStations?LineCode=\(lineCode!)"
         self.jsonGetRequestWithPath(path, success: { (json:JSON) in
@@ -53,12 +62,25 @@ public extension WMATASwift {
             }, failure: failure)
     }
     
-    public func getStationInformation(stationCode: String?, success: (Station)->Void, failure:(NSError)->Void) {
-        self.jsonGetRequestWithPath("Rail.svc/json/jStationInfo?StationCode=\(stationCode ?? "")", success: { (json:JSON) in
+    /**
+        Get Station Information
+     
+        - parameter (String) stationCode: Station code to get station information for
+     
+    */
+    public func getStationInformation(stationCode: String, success: (Station)->Void, failure:(NSError)->Void) {
+        self.jsonGetRequestWithPath("Rail.svc/json/jStationInfo?StationCode=\(stationCode)", success: { (json:JSON) in
             success(Station(json: json))
             }, failure: failure)
     }
     
+    /**
+        Get Path Between Stations
+     
+        - parameter (String) fromStationCode: Station code for beginning stop
+        - parameter (String) toStationCode: Station code for ending stop
+     
+    */
     public func getPathBetweenStations(fromStationCode: String, toStationCode: String, success: ([RailPath])->Void, failure: (NSError)->Void) {
         self.jsonGetRequestWithPath("Rail.svc/json/jPath?FromStationCode=\(fromStationCode)&ToStationCode=\(toStationCode)", success: { (json:JSON) in
             if let pathJson = json["Path"].array {
@@ -71,6 +93,12 @@ public extension WMATASwift {
             }, failure: failure)
     }
     
+    /**
+        Get Station To Station Information
+     
+        - parameter (String?) fromStationCode: Station code for beginning stop
+        - parameter (String?) toStationCode: Station code for ending stop
+    */
     public func getStationToStationInfo(fromStationCode: String?, toStationCode: String?, success: ([StationToStation])->Void, failure: (NSError)->Void) {
         self.jsonGetRequestWithPath("Rail.svc/json/jSrcStationToDstStationInfo?FromStationCode=\(fromStationCode ?? "")&ToStationCode=\(toStationCode ?? "")", success: { (json:JSON) in
             if let infoJson = json["StationToStationInfos"].array {
